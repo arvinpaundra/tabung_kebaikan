2ae2a9ca445e0f75fadb4e6ff7997a843344651e
arvinpaundra <arvinpaundra06@gmail.com>
2022-07-30 01:05:13 +0700

    Add input kondisi tabung at penarikan

--- components/organisms/CardPenarikan/FormPenarikan.jsx
+++ components/organisms/CardPenarikan/FormPenarikan.jsx
@@ -9,6 +9,7 @@ const FormPenarikan = (props) => {
   const { id_kec, id_munfiq, kode, user } = props;
 
   const [nominal, setNominal] = useState(0);
+  const [kondisi, setKondisi] = useState('');
 
   const router = useRouter();
 
@@ -20,6 +21,7 @@ const FormPenarikan = (props) => {
       id_munfiq: id_munfiq,
       nominal: +nominal,
       id_user: user.id_user,
+      kondisi_tabung: kondisi,
     };
 
     if (+nominal <= 0 || !nominal) {
@@ -53,6 +55,14 @@ const FormPenarikan = (props) => {
         decimalSeparator=","
         onValueChange={(values, sourceInfo) => setNominal(values.value)}
       />
+      <select
+        onChange={(event) => setKondisi(event.target.value)}
+        className="bg-light-grey px-4 py-2 text-black/90/80 rounded-full w-80 border-light-grey border hover:border-gsc focus:outline-none focus:border focus:border-gsc placeholder:text-black/90/80"
+      >
+        <option value="b">Baik</option>
+        <option value="r">Rusak</option>
+      </select>
+
       <button
         type="submit"
         className="bg-gsc/90 py-2 px-8 rounded-full w-fit self-end text-white hover:bg-gsc"
